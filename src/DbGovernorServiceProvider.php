8e0550bcf8be2c3513b797eae5c84ce8c04334b4
Abdullah Al Mamun <mamun167359@gmail.com>
2026-04-11 15:37:56 +0600

    feat: implement configuration merging and resource loading in DbGovernorServiceProvider

--- src/DbGovernorServiceProvider.php
+++ src/DbGovernorServiceProvider.php
@@ -6,8 +6,25 @@ use Illuminate\Support\ServiceProvider;
 
 class DbGovernorServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/db-governor.php', 'db-governor');
+    }
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'db-governor');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+
+        $this->publishes([
+            __DIR__.'/../config/db-governor.php' => config_path('db-governor.php'),
+        ], 'db-governor-config');
+
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/db-governor'),
+        ], 'db-governor-views');
+    }
 }
+
 
